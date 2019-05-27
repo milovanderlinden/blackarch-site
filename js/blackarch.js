227662f52db8ff6af3c9d2cdd5c7c510b129cd38
milovanderlinden <milo@dogodigi.net>
2019-05-27 10:45:50 +0200

    Update package list

--- js/blackarch.js
+++ js/blackarch.js
@@ -1,21 +1,3 @@
-/*$(document).off('click', '.act-bitcoin');
-$(document).on('click', '.act-bitcoin', function() {
-	$('.act-display').hide().empty();
-	$('.act-display').html('151ZXUgyVRRtzBeUEEJuksMiw5BD585eQL').fadeIn();
-});
-
-$(document).off('click', '.act-cash');
-$(document).on('click', '.act-cash', function() {
-	$('.act-display').hide().empty();
-	$('.act-display').html('Available soon').fadeIn();
-});
-
-$(document).off('click', '.act-cc');
-$(document).on('click', '.act-cc', function() {
-	$('.act-display').hide().empty();
-	$('.act-display').html('Available soon').fadeIn();
-});*/
-
 document.addEventListener('DOMContentLoaded', function () {
   var elem = document.querySelector('.sidenav');
   var sidenav = M.Sidenav.init(elem);
